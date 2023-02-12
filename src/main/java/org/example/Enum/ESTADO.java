eb6d866b5154ab8a5cf56d88f7534671404fa3b0
Rafael Wagner <rafaelwagner.rws@gmail.com>
2023-02-12 18:17:03 -0300

    remoção de comentario com anotações no ENUM estado

--- src/main/java/org/example/Enum/ESTADO.java
+++ src/main/java/org/example/Enum/ESTADO.java
@@ -2,36 +2,6 @@ package org.example.Enum;
 
 public enum ESTADO {
 
-/*
-    Acre (AC)
-    Alagoas (AL)
-    Amapá (AP)
-    Amazonas (AM)
-    Bahia (BA)
-    Ceará (CE)
-    Distrito Federal (DF)
-    Espírito Santo (ES)
-    Goiás (GO)
-    Maranhão (MA)
-    Mato Grosso (MT)
-    Mato Grosso do Sul (MS)
-    Minas Gerais (MG)
-    Pará (PA)
-    Paraíba (PB)
-    Paraná (PR)
-    Pernambuco (PE)
-    Piauí (PI)
-    Rio de Janeiro (RJ)
-    Rio Grande do Norte (RN)
-    Rio Grande do Sul (RS)
-    Rondônia (RO)
-    Roraima (RR)
-    Santa Catarina (SC)
-    São Paulo (SP)
-    Sergipe (SE)
-    Tocantins (TO)
-* */
-
     AC("Acre",2)
     ,AL("Alagoas",4)
     ,AP("Amapá",2)
